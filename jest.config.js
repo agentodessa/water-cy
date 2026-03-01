1e92586002fa55bd082b412deb91a00915e55829
Andrii Melnikov <andrii.melnikov@idealscorp.com>
2026-03-02 00:06:51 +0200

    fix: correct jest setupFilesAfterEnv key name

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,5 @@ module.exports = {
   transformIgnorePatterns: [
     'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/(?!.*node_modules).*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg|victory-native|react-native-reanimated|react-native-worklets)',
   ],
-  setupFilesAfterFramework: [],
+  setupFilesAfterEnv: [],
 };
